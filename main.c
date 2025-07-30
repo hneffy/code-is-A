5fa2ffffc2842ab46134ef58927424bfc0295a66
hneffy <princeofdarkness4545@gmail.com>
2025-07-31 01:22:02 +0500

    video 1
    make website

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h?>
+int main(int argc, char const *argv[])
+{
+    __DATE__doksmmdj
+    printf("hello world/n");
+    __OPENCL_MEMORY_SCOPE_DEVICE __DBL_DECIMAL_DIG__
+    return 0;
+}
